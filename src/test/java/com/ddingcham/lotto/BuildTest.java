e2b49c7ecc77d92dc2fef7971e68751f4ffc175b
ddingcham <qo0op308@gmail.com>
2018-10-29 20:52:15 +0900

    add LottoGeneratprTest // 기본 기능 요구사항

--- src/test/java/com/ddingcham/lotto/BuildTest.java
+++ src/test/java/com/ddingcham/lotto/BuildTest.java
@@ -4,10 +4,12 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class BuildTest {
+	
 	@Test
 	public void 의존성_테스트_Junit(){
 		final Object actual = null;
 		
 		Assert.assertNull(actual);
 	}
+	
 }
---
+++ src/test/java/com/ddingcham/lotto/LottoGeneratorTest.java
@@ -0,0 +1,17 @@
+package com.ddingcham.lotto;
+
+import org.junit.Test;
+
+public class LottoGeneratorTest {
+
+	@Test
+	public void 로또_구입_금액을_입력하면_구입_금액에_해당하는_로또를_발급해야_한다(){
+		
+	}
+	
+	@Test
+	public void 로또_1장의_가격은_1000원이다(){
+		
+	}
+	
+}
