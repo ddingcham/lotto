af4a6fac12a8a0828da4ee9928d3e3511ab41eef
ddingcham <qo0op308@gmail.com>
2018-10-30 21:59:34 +0900

    2차 클라이언트 구현 // 추가 요구사항 발생

--- src/main/java/com/ddingcham/lotto/LottoGame.java
+++ src/main/java/com/ddingcham/lotto/LottoGame.java
@@ -1,18 +1,47 @@
 package com.ddingcham.lotto;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+import ch.qos.logback.core.subst.Tokenizer;
+
 import com.ddingcham.lotto.domain.Lotto;
 import com.ddingcham.lotto.domain.Lottoes;
 
 public class LottoGame {
 	
+	private static Scanner scanner = new Scanner(System.in);
+	
 	public static void main(String[] args){
-		Lotto answer = Lotto.random();
-		Lottoes lottoes = Lottoes.generate(5000);
+		System.out.println("구입금액을 입력해 주세요.");
+		String price = scanner.nextLine().trim();
 		
-		System.out.println("answer : " + answer);
+		Lottoes lottoes = Lottoes.generate(Integer.parseInt(price));
+		
+		System.out.println(lottoes.size() + "개를 구매했습니다.");
+		System.out.println(lottoes);
+
+		System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+		String answerInput = scanner.nextLine().trim();
+		
+		Lotto answer = Lotto.ofInt(tokenizeAnswerNumbers(answerInput));
+		System.out.println(answer);
+		
+		System.out.println("당첨 통계");
+		System.out.println("-------");
+		
 		for(int i=0;i<lottoes.size();i++){
 			System.out.println(i + " lotto : " + lottoes.get(i));
 			System.out.println(answer.matchCount(lottoes.get(i)));
 		}
-	}	
+	}
+	
+	public static Integer[] tokenizeAnswerNumbers(String target){
+		String[] splitResult = target.split(",");
+		Integer[] result = new Integer[splitResult.length];
+		for(int i=0;i<splitResult.length;i++){
+			result[i] = Integer.valueOf(splitResult[i].trim());
+		}
+		return result;
+	}
 }
